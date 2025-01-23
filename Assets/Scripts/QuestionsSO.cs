82013d8ae86817dd62897d44a7a2e4c496a3e06f
Teodora Vela <teodora11vela@gmail.com>
2025-01-23 17:36:13 +0200

    added scriptable objects questions and answers; getter properties

---
+++ Assets/Scripts/QuestionsSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Quiz Question", fileName = "New Question")] //Asset Menu makes the object appear as an option in the Unity UI
+public class QuestionsSO : ScriptableObject
+{
+    [TextArea(2,6)] [SerializeField] string question;
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField]  int correctAnswerIndex;
+    public string Question { get { return question; }}
+    public string[] Answers { get { return answers; }}
+    public int CorrectAnswerIndex {  get { return correctAnswerIndex; }}
+}
